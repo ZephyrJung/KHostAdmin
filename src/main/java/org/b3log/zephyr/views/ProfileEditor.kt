82759f026e05210b79f81778018431bc23ec4368
yu.zhang <zephyrjung@126.com>
2018-01-08 21:26:02 +0800

    添加对话框，添加帮助文档

--- src/main/java/org/b3log/zephyr/views/ProfileEditor.kt
+++ src/main/java/org/b3log/zephyr/views/ProfileEditor.kt
@@ -1,16 +1,19 @@
 package org.b3log.zephyr.views
 
+import javafx.beans.property.SimpleStringProperty
+import javafx.geometry.Pos
 import javafx.scene.control.TableView
 import org.b3log.zephyr.controller.ProfileController
 import org.b3log.zephyr.model.Host
 import tornadofx.*
+import javax.swing.GroupLayout
 
 class ProfileEditor : View() {
     val controller: ProfileController by inject()
     var hostTable: TableView<Host> by singleAssign()
 
     override val root = form {
-        prefWidth=400.0
+        prefWidth = 400.0
         /*fieldset("Profile Information") {
             field("Name") {
                 textfield(controller.selectedProfile.name)
@@ -27,20 +30,48 @@ class ProfileEditor : View() {
                     hostTable = this
                     isEditable = true
                     smartResize()
-                    column("",Host::enable).makeEditable().minWidth(20)
+                    column("",Host::enable).useCheckbox(editable = true).minWidth(20)
                     column("IP地址", Host::ip).minWidth(150)
                     column("域名", Host::domain).minWidth(150)
                     itemsProperty().bind(controller.selectedProfile.hosts)
                 }
-                button("Add host") {
-                    setOnAction {
-                        val newHost = Host(true, "","",-1)
-                        controller.selectedProfile.hosts.value.add(newHost)
-                        hostTable.selectionModel.select(newHost)
-                        hostTable.edit(hostTable.items.size - 1, hostTable.columns.first())
+                buttonbar {
+                    button("Add") {
+                        setOnAction {
+                            //todo 我脚的这样写不太对吼。。。
+                            dialog("添加Host(默认启用)") {
+                                minWidth = 200.0
+                                prefWidth = 200.0
+                                val model = ViewModel()
+                                val ip = model.bind { SimpleStringProperty() }
+                                val domain = model.bind { SimpleStringProperty() }
+                                field("IP") {
+                                    textfield(ip) {
+                                        required()
+                                    }
+                                }
+                                field("域名") {
+                                    textfield(domain) {
+                                        required()
+                                    }
+                                }
+                                buttonbar {
+                                    button("Save").action {
+                                        model.commit { doSave(ip.value, domain.value) }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+
+    private fun doSave(ip: String, domain: String) {
+        val newHost = Host(true, ip, domain, controller.selectedProfile.id.value)
+        controller.selectedProfile.hosts.value.add(newHost)
+        hostTable.selectionModel.select(newHost)
+        hostTable.edit(hostTable.items.size - 1, hostTable.columns.first())
     }
 }
